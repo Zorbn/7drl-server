8cf8d21a488a5a2dab507f8c6188193049d607a7
Nicolas Berggren <nberggren4@gmail.com>
2022-03-08 15:54:09 -0800

    Init

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import * as SocketIo from "socket.io";
+import * as Http from "http";
+import express from "express";
+import path from "path";
+
+const port = 8080;
+
+const app = express();
+const httpServer = Http.createServer(app);
+const io = new SocketIo.Server(httpServer);
+
+io.on("connection", (socket: SocketIo.Socket) => {
+
+});
+
+app.use(
+    express.static(path.join(__dirname, "../../7drl-client/dist/"))
+);
+
+/*
+app.get('/', (req, res) => {
+   res.sendFile(__dirname + '../../7drl-client/dist/index.html');
+});
+ */
+
+httpServer.listen(port, () => {
+    console.log(`Listening on *:${port}`)
+});
